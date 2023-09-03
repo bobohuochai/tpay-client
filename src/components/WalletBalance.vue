1c00abc2d8438e999986ea116800203cc9ad6e7d
xujun <feng1987a@163.com>
2023-09-03 13:57:30 +0800

    fix: revert walletBalance;

--- src/components/WalletBalance.vue
+++ src/components/WalletBalance.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class="balance-wrap pt-48px  px-16px">
-        <span class="text-16px font-600">多币种钱包</span>
-        <ul>
-            <li v-for="item in wallets" :key="item.id" class="px-16px py-6px bg-#fff flex items-center justify-between">
-                <img class="h-24px w-24px" :src="currencyFile(`/src/assets/currency/${item.currencyCode}.svg`)" alt="" />
-                <div class="flex items-center">
-                    <span class="text-16px font-500 color-#2C261B mr-12px">{{ item.usableQuota }}</span>
-                    <span class="text-12px font-400 color-#2C261B">{{ item.currencyCode }}</span>
-                </div>
-            </li>
-        </ul>
-    </div>
+    <ul>
+        <li v-for="item in wallets" :key="item.id" class="px-16px py-6px bg-#fff flex items-center justify-between">
+            <img class="h-24px w-24px mr-16px" :src="currencyFile(`/src/assets/currency/${item.currencyCode}.png`)"
+                alt="" />
+            <div class="flex items-center">
+                <span class="text-16px font-500 color-#2C261B mr-8px">{{ amountFormat(item.usableQuota) }}</span>
+                <span class="text-12px font-400 color-#2C261B text-opacity-70">{{ item.currencyCode }}</span>
+            </div>
+        </li>
+    </ul>
 </template>
 
 <script setup>
